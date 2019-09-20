dd0a0b00b43278d04a4f70868cd6c4d80c19c8c5
Pavel <theoreticalboy.pf94@gmail.com>
2019-09-20 09:21:50 +0300

    Добавил небольшие комментарии облегчающие беглый просмотр после длительного времени

--- Python/try2.py
+++ Python/try2.py
@@ -2,13 +2,15 @@
 # gramma is Expression = Expression Operation Expression or Operation(Expression)
 
 class Expression:
+    """
+    interface class over possible math expression.
+    """
     operation = ''
     arguments = []
     def __init__(self, oper, args):
         self.operation = oper
         self.arguments = args
     def D(self):
-        #return self.operation.D(self.arguments)
         return self.operation.D(self.arguments)
     
     def __repr__(self):
@@ -16,20 +18,26 @@ class Expression:
 
     def simplify(self):
         """
-        element vise use simplify methot for each extention element
+        element vise use simplify method for each extention element
         """
-        #return self.operation.simplify(map(simplify,self.arguments))
         #print(self.arguments, list(map(type,self.arguments)))
         return self.operation.simplify(list(map(lambda x: x.simplify(), self.arguments)))
 
  
 class Operation:
+    """
+    interface class over possible operation.
+    """
     def simplify(self,args):
         return Expression(self, args)
     def D(self, args):
         pass
 
 class Function_sin (Operation):
+    """
+    D(sin(x)) = cos(x);
+    sin'(f(x)) == cos(f(x)) * f'(x) == cos(f(x)) * D(f(x));
+    """
     def D(self,args):
        exp = Expression(Function_cos(), args)
        return Expression(MULop(), [exp, args[0].D()])
@@ -41,6 +49,10 @@ class Function_sin (Operation):
        return "sin(%s)" % (args[0])
 
 class Function_cos (Operation):
+    """
+    D(cos(x)) = -sin(x);
+    cos'(f(x)) == -sin(f(x)) * f'(x) == -sin(f(x)) * D(f(x));
+    """
     def D(self, args):
         exp = Expression(Function_sin(), args)
         tmp = Expression(MULop(), [exp, args[0].D()])
@@ -53,6 +65,11 @@ class Function_cos (Operation):
         return "cos(%s)" % (args[0])
 
 class VAR(Expression):
+    """
+    pure x, without anything else;
+    may be in future that will cause trouble
+    becouse I try to add power ^ operation
+    """
     def simplify(self):
         return self
     def __init__(self):
@@ -77,6 +94,9 @@ class CONST_EXPR(Expression):
 class ADDop (Operation):
    
     def simplify(self, args):
+        """
+        0 + something == something + 0 == something
+        """
         if type(args[0]) is CONST_EXPR:
             if args[0].val == 0:
                 return args[1]
@@ -94,6 +114,9 @@ class ADDop (Operation):
 class SUBop (Operation):
 
     def simplify(self, args):
+        """
+        sence equal to ADDop.simplify
+        """
         if type(args[0]) is CONST_EXPR:
             if args[0].val == 0:
                 return Expression(MULop(), [CONST_EXPR(-1), args[1]])
@@ -109,6 +132,9 @@ class SUBop (Operation):
 class MULop (Operation):
     
     def simplify(self, args):
+        """
+        0 * something == something * 0 == 0;
+        """
         if type(args[0]) is CONST_EXPR:
             if args[0].val == 0:
                 return args[0]
